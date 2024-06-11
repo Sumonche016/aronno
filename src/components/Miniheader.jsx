968629e9438bfb276cc23491e764305e1eaaa471
Sumon Bala <Sumonche016@gmail.com>
2024-06-11 15:41:54 +0600

    new aded

--- src/components/Miniheader.jsx
+++ src/components/Miniheader.jsx
@@ -2,7 +2,7 @@ import { BsHeadset } from "react-icons/bs";
 
 const Miniheader = () => {
   return (
-    <div className="border-t border-borderPrimar text-sm hidden md:inline-block md:text-[1rem]">
+    <div className="border-t border-borderPrimar w-full text-sm   hidden md:inline-block md:text-[1rem]">
       <div className="py-2 container flex justify-between items-center">
         <div>
           <ul className="flex items-center gap-4 text-[1.1rem]">
